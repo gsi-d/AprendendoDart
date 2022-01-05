ee3f12fb6ff5a9dab1c7f78e09e5e88aed2ec8ae
Guilherme <guimota_silver@hotmail.com>
2022-01-05 15:35:57 -0300

    parâmetros nomeados e retorno de funções

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,8 @@
-int sum(int n1, [int n2]) {
-  return n1 + (n2 ?? 5);
-}
+import 'package:meta/meta.dart';
+
+int sum({@required int n1, @required int n2}) => n1 + n2;
 
 void main() {
-  var n = sum(1);
+  var n = sum(n1: 1, n2: 5);
   print(n);
 }
